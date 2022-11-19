2ec3a49a1a9fab5394bdcc0154ec38d7961f948b
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-11-20 02:01:12 +0530

    New Web commit Stage : 2

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+const colour = require('colors');
 var express = require('express')
-var app = express()
+const http = require('http');
+const { Server } = require('socket.io');
+var app = express();
+const servers = http.createServer(app)
+const io = new Server(server)
 
-app.get('/', (req, res) => {
-    res.send('haiiii')
-})
-
+app.use(express.static('./public'))
 
 process.env.PORT | 3000
 
+app.get('/', (req, res) => {
+    res.render('./index.html')
+})
 
-var server = app.listen(process.env.PORT || 8081, () => {
-    console.log('Server is started on 127.0.0.1:' + (process.env.PORT || 8081), `http://localhost:8081/`)
+
+
+
+var server = servers.listen(process.env.PORT || 8081, () => {
+    console.log('Server is started on 127.0.0.1:' + (process.env.PORT || 8081), `http://localhost:8081/`.red)
 })

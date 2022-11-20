d173511f3cda425966848b887271fece6c4ec4b8
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-11-21 01:33:26 +0530

    user is connected to socket

--- app.js
+++ app.js
@@ -3,16 +3,43 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var coloures = require('colours')
+var colours = require('colours')
 
-
+var http = require('http');
 
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// const { Server } = require('https');
 
 var app = express();
+
+
+/**
+ * Create  socket.io .
+ */
+
+var server = http.createServer(app);
+var { Server } = require("socket.io");
+var io = new Server(server);
+
+
+
+const registerOrderHandlers = require("./routes/index");
+const registerUserHandlers = require("./routes/users");
+
+const onConnection = (socket) => {
+  registerOrderHandlers(io, socket);
+  registerUserHandlers(io, socket);
+}
+
+
+
+
+io.on("connection", onConnection)
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +52,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// app.set(server)
+// app.set(express.Server)
+// app.set('socketio');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
-
+var server = require('../bin/www');
+// 
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.render('index')
 
-
-
 });
 
+// io.on('connection', (socket) => {
+
+// })
+
+
+
+module.exports = (io, socket) => {
+
+}
 
 
 module.exports = router;
